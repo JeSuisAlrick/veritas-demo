0f1ed5831a4d78880a25d5e03f32a5938dba10d3
Alrick Telfer <alrick@unihost.com.jm>
2018-09-25 17:00:53 -0500

    Add security and general configurations

--- src/main/java/com/vertis/demo/config/BasicAuthEntryPoint.java
+++ src/main/java/com/vertis/demo/config/BasicAuthEntryPoint.java
@@ -12,11 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author telfealr
  */
+@Component
 public class BasicAuthEntryPoint extends BasicAuthenticationEntryPoint {
 
     @Override
